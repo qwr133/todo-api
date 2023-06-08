d4c897d354705ee06291ff1fac95fb0ae3c3cee0
qwr133 <121946724+qwr133@users.noreply.github.com>
2023-06-08 12:46:48 +0900

    config 재설정

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -2,15 +2,33 @@ package com.example.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+        ;
+
+        return http.build();
     }
 
 }
