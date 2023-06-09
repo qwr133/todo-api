a1c6b19925da36de259daacfe497c750cda8b54d
qwr133 <121946724+qwr133@users.noreply.github.com>
2023-06-09 10:40:42 +0900

    :memo: user 패키지, custom exception  작성

---
+++ src/main/java/com/example/todo/exception/DuplicatedEmailException.java
@@ -0,0 +1,10 @@
+package com.example.todo.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DuplicatedEmailException  extends RuntimeException {
+    public DuplicatedEmailException(String message) {
+        super(message);
+    }
+}
