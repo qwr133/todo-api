53e1daac72eb9168b716ef1cdf2bb8a17aca6d15
qwr133 <ksg165000@gmail.com>
2023-07-30 20:51:20 +0900

    :memo: user 회원가입 exception, corsConfig

--- src/main/java/com/example/todo/exception/DuplicatedEmailException.java
+++ src/main/java/com/example/todo/exception/DuplicatedEmailException.java
@@ -3,7 +3,9 @@ package com.example.todo.exception;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class DuplicatedEmailException  extends RuntimeException {
+public class DuplicatedEmailException
+        extends RuntimeException {
+
     public DuplicatedEmailException(String message) {
         super(message);
     }
--- src/main/java/com/example/todo/exception/NoRegisteredArgumentsException.java
+++ src/main/java/com/example/todo/exception/NoRegisteredArgumentsException.java
@@ -3,9 +3,10 @@ package com.example.todo.exception;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class NoRegisteredArgumentsException extends RuntimeException {
+public class NoRegisteredArgumentsException
+        extends RuntimeException {
 
-    //기본 생성자 _ 에러메세지를 받는 생성자
+    // 기본 생성자 + 에러메시지를 받는 생성자
     public NoRegisteredArgumentsException(String message) {
         super(message);
     }
